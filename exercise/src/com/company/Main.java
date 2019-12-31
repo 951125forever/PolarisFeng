db08b0742fdea6ce7db3ad453a35c2da0b7032fb
951125forever <17612130657@163.com>
2019-12-31 16:48:22 +0800

    exercisetest2222

--- exercise/src/com/company/Main.java
+++ exercise/src/com/company/Main.java
@@ -1,11 +1,12 @@
 package com.company;
 
-import java.util.ArrayList;
+import java.beans.Transient;
 import java.util.HashMap;
 
 public class Main {
-
+    @Transient
     public static void main(String[] args) {
+
         HashMap map = new HashMap();
         map.put("1","123");
         map.put("1","456");
